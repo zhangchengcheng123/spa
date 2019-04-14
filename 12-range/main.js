9b56ddc13a451fd9dcad2b08d70e4b29b7aea4bb
zhangchengcheng123 <1306073684@qq.com>
2019-04-14 12:02:25 +0800

    12-range

---
+++ 12-range/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    var $range=$('input[type="range"]');
+    var $txtAge=$('#age');
+    $range.change(function(){
+    //console.log($range.val());
+    //console.log($txtAge.html());
+    //        $txtAge.html($range.val()); //val是上面的字
+    //          
+});
+
+});
